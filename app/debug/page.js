729a0a495fb127c8d0ed2bea96edab232a565c11
BizzBuzzCreations <bizzbuzzcreation@gmail.com>
2026-02-11 11:32:41 +0530

    changes

--- app/debug/page.js
+++
@@ -1,7 +0,0 @@
-import { testEnv } from "@/actions/serverActions";
-
-export default async function DebugPage() {
-  const env = await testEnv();
-
-  return <pre>{JSON.stringify(env, null, 2)}</pre>;
-}
